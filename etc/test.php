a6f69dc4114ed9c2206c6202480f483118ee5e52
Tom Rinaldo <emuzhatesms@gmail.com>
2011-12-08 21:34:01 -0500

    Cleanup of etc/;Added w00t and heart from ABI

--- etc/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('lib/common.php');
-
-echo "<embed id='bombsnd' src='etc/LTTP_Bomb_Blow.wav' loop='false' autostart='true' type='audio/x-wav' />";
-
-echo decryptpwd("qb3fsxjgUKIDs//RxEizPymqn29g/PHmHYW3X5LfUqw=");
-
-?>

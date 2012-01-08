e61b20e825294dfec9a906fc1c3a74cf2dc8c1a2
Kafuka Live Board <dev@kafuka.org>
2012-01-09 02:03:48 +1100

    activeusers.php changes

--- public_html/activeusers.php
+++ public_html/activeusers.php
@@ -41,7 +41,8 @@
 
   pagefooter();
 
-  function timelink($time){
-    return " <a href=activeusers.php?time=$time>".timeunits2($time).'</a> ';
+  function timelink($timex){
+    global $time;
+    return ($time==$timex ? " ".timeunits2($timex)." " : " <a href=activeusers.php?time=$timex>".timeunits2($timex).'</a> ');
   }
 ?>

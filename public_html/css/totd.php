3928c50150efd933ce03fa0ffe1df3a8dd467b0d
Gywall <draobyek@googlemail.com>
2012-02-21 00:11:43 +0000

    fixing a hard test value

--- public_html/css/totd.php
+++ public_html/css/totd.php
@@ -7,7 +7,7 @@ $themes=unserialize(file_get_contents("../themes_serial.txt"));
 $d=getdate();
 $l=count($themes)-1;
 $t=floor($d[0]/86400)%$l;
-if($themes[$t][0]=="Theme of the Day") $t=35;
+if($themes[$t][0]=="Theme of the Day") $t=$l;
 if(is_file($themes[$t][1].".css")){ $ext = ".css"; } else { if(is_file($themes[$t][1].".php")){ $ext = ".php"; } else { header("Location: 0.css"); die(); } }
 header("Location: ".$themes[$t][1]."$ext");
 ?>

e29fb2244e9ceb09ecc0f97032ca104af7b0d117
Tom Rinaldo <emuzhatesms@gmail.com>
2013-06-10 07:00:43 -0400

    Added link to development page on 'Powered by' Image

--- public_html/lib/common.php
+++ public_html/lib/common.php
@@ -5,8 +5,8 @@
   
   //[Scrydan] Added these three variables to make editing quicker.
   $boardprog = "Acmlm, Emuz, <a href='credits.php'>et al</a>.";
-  $abdate    = "5/24/2013";
-  $abversion = "2.5.2 (<i><font color='#FFEA95'>Development</font></i>)";
+  $abdate    = "6/10/2013";
+  $abversion = "2.5.2 (<i><font color='#F185C9'>PRE-RELEASE</font></i>)";
 
   $userip  = $_SERVER['REMOTE_ADDR'];
   $userfwd = addslashes(getenv('HTTP_X_FORWARDED_FOR')); //We add slashes to that because the header is under users' control
@@ -689,7 +689,7 @@
     //pagestats();
 
     print "<br>
-           $L[TBL2]>$L[TRc]>$L[TD2l]><center><img src=\"img/poweredbyacmlm.PNG\"><br>
+           $L[TBL2]>$L[TRc]>$L[TD2l]><center><a href=\"https://bitbucket.org/acmlmboard/acmlmboard-2\" title=\"Acmlmboard 2\"><img src=\"img/poweredbyacmlm.PNG\"></a><br>
              Acmlmboard v$abversion ($abdate)<br>
              &copy; 2005-2013 $boardprog
            $L[TBLend]";

0c66ac4c49ede1369d4ffbb50942725c9e434d39
Tom Rinaldo <emuzhatesms@gmail.com>
2013-06-27 20:59:15 -0400

    Removing development tag from master

--- public_html/lib/common.php
+++ public_html/lib/common.php
@@ -6,7 +6,7 @@
   //[Scrydan] Added these three variables to make editing quicker.
   $boardprog = "Acmlm, Emuz, <a href='credits.php'>et al</a>.";
   $abdate    = "6/27/2013";
-  $abversion = "2.5.2 <span style=\"color: #BCDE9A; font-style: italic;\">Development</span>";
+  $abversion = "2.5.2";
 
   $userip  = $_SERVER['REMOTE_ADDR'];
   $userfwd = addslashes(getenv('HTTP_X_FORWARDED_FOR')); //We add slashes to that because the header is under users' control

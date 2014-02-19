9c1e606c109ce5dbc521b24de9cc1112e04f0769
Gywall <draobyek@googlemail.com>
2014-02-19 22:51:34 +0000

    Support for "quotedby" in css postlayouts.

--- public_html/lib/post.php
+++ public_html/lib/post.php
@@ -178,9 +178,9 @@
 
     $msg=preg_replace("'\[url=(.*?)\](.*?)\[/url\]'si",'<a href=\\1>\\2</a>',$msg);
 
-    $msg=preg_replace("'\[reply=\"(.*?)\" id=\"(.*?)\"\]'si",'<blockquote><small><i><a href=showprivate.php?id=\\2>Sent by \\1</a></i></small><hr>',$msg);
-    $msg=preg_replace("'\[quote=\"(.*?)\" id=\"(.*?)\"\]'si",'<blockquote><small><i><a href=thread.php?pid=\\2#\\2>Posted by \\1</a></i></small><hr>',$msg);
-    $msg=preg_replace("'\[quote=(.*?)\]'si",'<blockquote><i>Posted by \\1</i><hr>',$msg);
+    $msg=preg_replace("'\[reply=\"(.*?)\" id=\"(.*?)\"\]'si",'<blockquote><span class="quotedby"><small><i><a href=showprivate.php?id=\\2>Sent by \\1</a></i></small></span><hr>',$msg);
+    $msg=preg_replace("'\[quote=\"(.*?)\" id=\"(.*?)\"\]'si",'<blockquote><span class="quotedby"><small><i><a href=thread.php?pid=\\2#\\2>Posted by \\1</a></i></small></span><hr>',$msg);
+    $msg=preg_replace("'\[quote=(.*?)\]'si",'<blockquote><span class="quotedby"><i>Posted by \\1</i></span><hr>',$msg);
     $msg=preg_replace("'>>([0-9]+)'si",'>><a href=thread.php?pid=\\1#\\1>\\1</a>',$msg);
     //dynamically convert SSL and non-SSL links
     if(isssl()) $msg=str_replace($config[base],$config[sslbase],$msg);

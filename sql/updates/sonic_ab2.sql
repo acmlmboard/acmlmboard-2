a7a647f69cf1570126e2ee9c3ed72df66f991e91
Tom Rinaldo <emuz@emuz.us>
2014-11-28 18:00:29 -0500

    Added Sonic Ranks by Danika

---
+++ sql/updates/sonic_ab2.sql
@@ -0,0 +1,25 @@
+INSERT INTO `bananafan`.`ranksets` (
+`id` ,
+`name`
+)
+VALUES (
+'4', 'Sonic (by Danika)'
+);
+INSERT INTO `ranks` (`rs`, `p`, `str`) VALUES
+(4, 0, 'Newcomer'),
+(4, 10, '<img src=img/rankss/ring.gif>'),
+(4, 50, '<img src=img/rankss/ce-blue.png>'),
+(4, 80, '<img src=img/rankss/ce-purple.png>'),
+(4, 110, '<img src=img/rankss/ce-red.png>'),
+(4, 140, '<img src=img/rankss/ce-pink.png>'),
+(4, 170, '<img src=img/rankss/ce-yellow.png>'),
+(4, 200, '<img src=img/rankss/ce-green.png>'),
+(4, 230, '<img src=img/rankss/ce-grey.png>'),
+(4, 260, '<img src=img/rankss/se-blue.png>'),
+(4, 350, '<img src=img/rankss/se-purple.png>'),
+(4, 440, '<img src=img/rankss/se-red.png>'),
+(4, 530, '<img src=img/rankss/se-pink.png>'),
+(4, 620, '<img src=img/rankss/se-yellow.png>'),
+(4, 710, '<img src=img/rankss/se-green.png>'),
+(4, 800, '<img src=img/rankss/se-grey.png>'),
+(4, 1000, '<img src=img/rankss/master.png>');

b4b7b031d5365d6bd2daf6c922787dbd7a5bcd52
SquidEmpress <squidempress2@gmail.com>
2014-12-13 06:14:25 +0000

    perm_update-141213.sql edited online with Bitbucket

--- sql/updates/perm_update-141213.sql
+++ sql/updates/perm_update-141213.sql
@@ -1,4 +1,6 @@
-#Adds the manage-shop-items perm
-#Date 12/12/2014
+#Adds the manage-shop-items perm and support for logo override bypass
+#Date 12/13/2014
 
 INSERT INTO `perm` (`id`, `title`, `description`, `permcat_id`, `permbind_id`) VALUES ('manage-shop-items', 'Manage Shop Items', '', '3', '');
+INSERT INTO `perm` (`id`, `title`, `description`, `permcat_id`, `permbind_id`) VALUES
+('bypass-logo-override', 'Bypass Logo Overrides', 'Bypasses any board-wide logo locks.', 3, '');

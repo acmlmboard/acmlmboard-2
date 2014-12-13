4f9b49adf6ad4becf9cdf7bac21daad9e616eb3c
SquidEmpress <squidempress2@gmail.com>
2014-12-13 04:21:45 +0000

    Added the permission for being able to choose to be shown online or not in editprofile.php

---
+++ sql/updates/perm_update-141212.sql
@@ -0,0 +1,4 @@
+#Adds the show-online controlling perm
+#Date 12/12/2014
+
+INSERT INTO `perm` (`id`, `title`, `description`, `permcat_id`, `permbind_id`) VALUES ('show-online', 'Show Online', '', '1', '');

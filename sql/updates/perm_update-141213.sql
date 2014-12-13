0ed8ffc39e3627b9f56bd7d370f3bc505ddee5f6
SquidEmpress <squidempress2@gmail.com>
2014-12-13 05:11:36 +0000

    Item Shop managing and whatnot

---
+++ sql/updates/perm_update-141213.sql
@@ -0,0 +1,4 @@
+#Adds the manage-shop-items perm
+#Date 12/12/2014
+
+INSERT INTO `perm` (`id`, `title`, `description`, `permcat_id`, `permbind_id`) VALUES ('manage-shop-items', 'Manage Shop Items', '', '3', '');

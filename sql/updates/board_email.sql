789f8df78e70c6c5d20b48a4f0554a9dfb67221d
SquidEmpress <squidempress2@gmail.com>
2014-12-15 21:15:55 +0000

    Made into a column in misc rather than a completely new table called board_email.

--- sql/updates/board_email.sql
+++ sql/updates/board_email.sql
@@ -1,8 +1,4 @@
-#Creates a table for the email address that is shown to users who cannot register due to the proxy protection.
-#Date: 12/14/2014
-CREATE TABLE `board_email` (
-`field` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-`emailaddress` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
-) ENGINE = MYISAM ;
-
-INSERT INTO `board_email` (`field`, `emailaddress`) VALUES ('boardemail', '0');
+#Adds a column for the email address that is shown to users who cannot register due to the proxy protection.
+#Date: 12/15/2014
+ALTER TABLE `misc` ADD `emailaddress` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0' AFTER `txtval`;
+INSERT INTO `misc` (`field`, `emailaddress`) VALUES ('boardemail', '0');

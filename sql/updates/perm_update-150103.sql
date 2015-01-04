e9a66bd39c35e6ba54a46f1c0776c39f65773aa3
SquidEmpress <squidempress2@gmail.com>
2015-01-04 01:38:56 +0000

    Added perm for editing the Hide from Online Views option.

--- sql/updates/perm_update-150103.sql
+++ sql/updates/perm_update-150103.sql
@@ -1,4 +1,5 @@
-#Adds perm to allow access to the admin tools. 
+#Adds perm to allow access to the admin tools and for editing the show online feature in user profiles. 
 #Date 01/03/2015
 
 INSERT INTO `perm` (`id`, `title`, `description`, `permcat_id`, `permbind_id`) VALUES ('admin-tools-access', 'Access to Admin Tools', '', '3', '');
+INSERT INTO `perm` (`id`, `title`, `description`, `permcat_id`, `permbind_id`) VALUES ('edit-user-show-online', 'Edit User Show Online', '', '3', '');

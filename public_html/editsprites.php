2dc70101354c01d84ccbbd0645994bacaf7c4e9e
SquidEmpress <squidempress2@gmail.com>
2015-01-08 05:46:19 +0000

    Added a Sprite Category Editor to editsprites.php

--- public_html/editsprites.php
+++ public_html/editsprites.php
@@ -32,7 +32,164 @@ else {
     }
   }
 
-  if(empty($r['action'])) {
+  if(isset($_GET['spritecategories'])) {
+
+ if(empty($r['action'])){
+
+$headers = array
+(
+	"id" => array
+	(
+		"caption" => "#",
+		"width" => "32px",
+		"align" => "center",
+		"color" => 1
+	),
+	"name" => array("caption"=>"Name", "align"=>"center", "color"=>1),
+"edit" => array("caption"=>"Actions","color"=>1),
+);
+
+$data = array();
+$scReq = $sql->query("SELECT * FROM spritecateg ORDER BY id ASC");
+while($sc = $sql->fetch($scReq))
+{
+$actions = array(
+  array('title' => 'Edit','href' => 
+'editsprites.php?spritecategories&action=edit&id='.$sc['id']),
+  array('title' => 'Delete','href' => 
+'editsprites.php?spritecategories&action=del&id='.$sc['id'], 
+confirm => true),
+);
+		
+$data[] = array
+		(
+			"id" => $sc['id'],
+			"name" => $sc['name'],
+"edit" => RenderActions($actions,1),
+		);
+}
+$pagebar['breadcrumb'] = array(
+    array('title' => 'Edit Sprites','href' => 'editsprites.php'),
+    );
+$pagebar['title'] = 'Edit Sprite Categories';
+$pagebar['actions'] = array(
+    array('title' => 'New Sprite','href' => 'editsprites.php?action=new'), array('title' => 'New Sprite Category','href' => 'editsprites.php?spritecategories&action=new'),
+);
+RenderPageBar($pagebar);
+RenderTable($data, $headers);
+}
+
+  if ($r['action'] == "del") {
+    unset($r['action']);
+      if ($sql->prepare('DELETE FROM spritecateg WHERE id=?',array($id))) {
+      $pagebar['message'] = "Sprite category successfully deleted.";
+ }
+else {
+ $pagebar['message'] = "Unable to delete sprite category.";
+}
+  }
+
+ elseif ($r['action']=="edit" || $r['action']=="new") {
+if (!empty($r['act'])) {
+      $spca =
+request_variables(array('name'));
+
+
+if ($r['action']=="edit") {
+
+if(      $sql->prepare('UPDATE spritecateg SET 
+name=? WHERE id=?;', array(
+$spca['name'],
+$id,
+)
+)){
+      $pagebar['message'] = "Sprite category successfully updated.";
+
+}
+else {
+ $pagebar['message'] = "Unable to update  sprite category.";
+
+}
+}
+
+elseif ($r['action']=="new"){
+if (      $sql->prepare('INSERT INTO spritecateg SET
+name=? ;', array(
+$spca['name'],
+)
+)) {
+$id = $sql->insertid();
+$r['action'] = "edit";
+      $pagebar['message'] = "Sprite category successfully created.";
+}
+else {
+ $pagebar['message'] = "Unable to create sprite category.";
+}
+}
+}
+$pagebar['breadcrumb'] = array(
+    array('title' => 'Edit Sprites','href' => 'editsprites.php'), array('title' => 'Edit Sprite Categories','href' => 'editsprites.php?spritecategories'),
+    );
+
+   if ($id > 0) { 
+   $tsc=$sql->fetchp('SELECT * FROM spritecateg WHERE id=?',array($id));
+$pagebar['title'] = $tsc['name'];
+$pagebar['actions'] = array(
+    array('title' => 'Delete Sprite Category','href' => 
+'editsprites.php?spritecategories&action=del&id='.$id, 
+'confirm' 
+=> 
+true),
+);
+
+}
+else {
+$pagebar['title'] = 'New Sprite Category';
+$tsc = array(
+  'id' =>0,
+  'name' => '',
+);
+}
+RenderPageBar($pagebar);
+$form = array(
+  'action' =>
+    urlcreate('editsprites.php', array(
+      'action' => $r['action'],
+      'id' => $tsc['id'],
+    )
+    ),
+  'method' => 'POST',
+  'categories' => array(
+    'metadata' => array(
+      'title' => 'Sprite Category Name',
+      'fields' => array(
+        'name' => array(
+          'title' => 'Name',
+          'type' => 'text',
+          'length' => 60,
+          'size' => 40,
+'value' => $tsc['name'],
+        ),
+),
+    ),
+    'actions' => array(
+      'fields' => array(
+        'act' => array(
+          //'title' => 'Update metadata':'Create sprite category',
+          'type' => 'submit',
+        ),
+      ),
+    ),    
+  ),
+);
+
+RenderForm($form);
+
+}
+
+}
+
+  elseif(empty($r['action'])) {
 
 $headers = array
 (
@@ -78,8 +235,7 @@ $data[] = array
 }
 $pagebar['title'] = 'Edit Sprites';
 $pagebar['actions'] = array(
-    array('title' => 'New Sprite','href' => 'editsprites.php?action=new'),
-);
+    array('title' => 'New Sprite','href' => 'editsprites.php?action=new'), array('title' => 'Sprite Categories','href' => 'editsprites.php?spritecategories'), 
 RenderPageBar($pagebar);
 RenderTable($data, $headers);
 

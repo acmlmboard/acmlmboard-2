1316ff743358e1d99da5e991d0f450c04a21f3f5
SquidEmpress <squidempress2@gmail.com>
2015-01-11 09:46:12 +0000

    Added option in Administrator Tools to edit the lockdown message

--- public_html/administratortools.php
+++ public_html/administratortools.php
@@ -12,6 +12,7 @@ if (!has_perm('admin-tools-access')) no_perm();
 if($_POST[action]=="Apply changes") {
 $sql->query("UPDATE misc SET intval='".$_POST[regdisable]."' WHERE field='regdisable'");
 $sql->query("UPDATE misc SET intval='".$_POST[lockdown]."' WHERE field='lockdown'");
+$sql->query("UPDATE misc SET txtval='".$_POST[txtval]."' WHERE field='lockdown'");
  
 print "$L[TBL1]>
 ".        "  $L[TD1c]>
@@ -24,6 +25,7 @@ die(pagefooter());
 
 $rtool  = $sql->resultq('SELECT `intval` FROM `misc` WHERE `field`="regdisable"'); 
 $ltool  = $sql->resultq('SELECT `intval` FROM `misc` WHERE `field`="lockdown"');
+$lockdowntext = $sql->resultq("SELECT txtval FROM misc WHERE field='lockdown'");
 
 print "<form action='administratortools.php' method='post' enctype='multipart/form-data'>
 ".        " $L[TBL1]>
@@ -31,6 +33,9 @@ print "<form action='administratortools.php' method='post' enctype='multipart/fo
            catheader('Administrator tools')."
 ".    fieldrow('Disable Registering', fieldoption('regdisable',$rtool['regdisable'],array('Enable', 'Disable')))."
 ".    fieldrow('Enable Lockdown', fieldoption('lockdown',$ltool['lockdown'],array('Do not set', 'Set')))."
+".        "  $L[TR]>
+".        "    $L[TD1c]>Lockdown Message (Leave blank for default):</td>
+".        "      $L[TD2]>$L[TXTa]='txtval' rows=8 cols=120>".$lockdowntext."</textarea></td>
 ".        "  $L[TR1]>
 ".        "    $L[TD]>&nbsp;</td>
 ".        "    $L[TD]>$L[INPs]=action value='Apply changes'></td>

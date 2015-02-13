4af469759444bdcaecb10725875ffe827ccdd389
SquidEmpress <squidempress2@gmail.com>
2015-02-13 14:57:39 +0000

    Oops

--- sql/updates/perm_update-150213.sql
+++ sql/updates/perm_update-150213.sql
@@ -1,5 +1,5 @@
 #Adds perm for banning users.
-#1/10/2015
+#2/13/2015
 
 INSERT INTO `perm` (`id`, `title`, `description`, `permcat_id`, `permbind_id`) VALUES ('edit-titles', 'Edit Titles', '', '3', '');
 INSERT INTO `x_perm` (`id`, `x_id`, `x_type`, `perm_id`, `permbind_id`, `bindvalue`, `revoke`) VALUES (LAST_INSERT_ID(), 4, 'group', 'edit-titles', '', 0, 0);

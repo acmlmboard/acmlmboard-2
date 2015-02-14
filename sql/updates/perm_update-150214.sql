780d41e30a74bacf7038eb80df4ba13b77b4a7ec
SquidEmpress <squidempress2@gmail.com>
2015-02-14 15:08:49 +0000

    Added a perm for editing calendar events

---
+++ sql/updates/perm_update-150214.sql
@@ -0,0 +1,4 @@
+#Adds perm editing calendar events.
+#2/14/2015
+
+INSERT INTO `perm` (`id` , `title` , `description` , `permcat_id` , `permbind_id`)VALUES ('edit-calendar-events', 'Edit Calendar Events', '', '3', '');

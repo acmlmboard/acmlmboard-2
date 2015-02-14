d32da5a01faa950938d83df0f722032df1bd73d3
SquidEmpress <squidempress2@gmail.com>
2015-02-14 16:43:00 +0000

    Added another perm update

--- sql/updates/perm_update-150214.sql
+++ sql/updates/perm_update-150214.sql
@@ -1,4 +1,5 @@
-#Adds perm editing calendar events.
+#Adds an editing calendar events perm and another extended profile fields perm.
 #2/14/2015
 
 INSERT INTO `perm` (`id` , `title` , `description` , `permcat_id` , `permbind_id`)VALUES ('edit-calendar-events', 'Edit Calendar Events', '', '3', '');
+INSERT INTO `perm` (`id`, `title`, `description`, `permcat_id`, `permbind_id`) VALUES ('update-user-extended-profile', 'Update User Extended Profile', '', '3', 'users');

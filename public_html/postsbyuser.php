8edc51cb76f3cf2f1ed8425d948d19a6f1931deb
SquidEmpress <squidempress2@gmail.com>
2015-02-16 23:27:12 +0000

    Posts by thread and posts by forum

--- public_html/postsbyuser.php
+++ public_html/postsbyuser.php
@@ -11,7 +11,7 @@
   
   if($page<1) $page=1;
 
-  if($id) {
+  else if($id) {
     $user=$sql->fetchq("SELECT id,name,displayname FROM users WHERE id=$id");
     if($user[id]) {
       
@@ -28,7 +28,7 @@
       $numposts=$numposts[c];
 
       $p=$sql->query("SELECT p.id,p.num,p.date,t.title,f.private FROM (posts p LEFT JOIN threads t ON t.id=p.thread) "
-                    ."LEFT JOIN forums f ON f.id=t.forum WHERE p.user=$id AND f.id IN ".forums_with_view_perm()
+                    ."LEFT JOIN forums f ON f.id=t.forum WHERE p.user=$id "
                     ."ORDER BY p.num DESC LIMIT ".(($page-1)*$loguser[tpp]).",".$loguser[tpp]);
 
       $i=0;
@@ -70,7 +70,102 @@
 ".         "$L[TBLend]";
   }
 
+  //This is heavily based off of AB1's code so posts by thread and posts by forum need to be cleaned at some point.
+  if(isset($_GET[postsbythread])) {
+  $time=$_GET[time];
+if(!$time) $time=86400;
+  $posters=$sql->query("SELECT t.id,t.replies,t.title,t.forum,f.private,COUNT(p.id) cnt FROM threads t,posts p,forums f WHERE p.user=$id AND p.thread=t.id AND p.date>".(ctime()-$time).' AND t.forum=f.id GROUP BY t.id ORDER BY cnt DESC');
+  $u=$sql->fetchq("SELECT name FROM users WHERE id=$id");
+  $username=$u[name];
+  $lnk="<a href=postsbyuser.php?postsbythread&id=$id&time";
+  if($time<999999999) $during=' during the last '.timeunits2($time);
+  $print= "$lnk=3600>1 hour</a> |
+	$lnk=86400>1 day</a> |
+	$lnk=604800>7 days</a> |
+	$lnk=2592000>30 days</a> | 
+	$lnk=999999999>Total</a><br>
+	Posts by $username in threads$during:
+".           "$L[TBL1]>
+".           "  $L[TRh]>
+".	"$L[TDh]>#
+".	"$L[TDh]>Thread
+".	"$L[TDh]>Posts
+".	"$L[TDh]>Thread total
+".      "  </tr>
+  ";
+  for($i=1;$t=$sql->fetch($posters);$i++){
+    $print.= "
+	<tr>
+".	"$L[TDc]>$i</td>
+".	"$L[TD] align=left>
+    ";
+    if(!(can_view_forum($t)))
+	$print.= "<i>(Restricted forum)</i>";
+    else $print.= "<a href=thread.php?id=$t[id]>$t[title]</a>";
+    $print.= "
+	</td>
+".	"$L[TDc]>$t[cnt]</td>
+".     "$L[TDc]>".($t[replies]+1)."</td>
+".     "  </tr>
+    ";
+  }
+      $print.="$L[TBLend]";
+            $fpagelist="";
+  }
+  
+  if(isset($_GET[postsbyforum])) {
+  $time=$_GET[time];
+  if(!$time) $time=86400;
+  if($id){
+    $useridquery="posts.user=$id AND";
+    $by='by ';
+    $u=$sql->fetchq("SELECT name FROM users WHERE id=$id");
+    $username=$u[name];
+  }
+  $posters=$sql->query("SELECT forums.*,COUNT(posts.id) AS cnt FROM forums,threads,posts WHERE $useridquery posts.thread=threads.id AND threads.forum=forums.id AND posts.date>".(ctime()-$time).' AND threads.announce=0 GROUP BY forums.id ORDER BY cnt DESC');
+  $userposts=$sql->query("SELECT id FROM posts WHERE $useridquery date>".(ctime()-$time).'');
+  $lnk="<a href=postsbyuser.php?postsbyforum&id=$id&time";
+  if($time<999999999) $during=' during the last '.timeunits2($time);
+  $print= "$lnk=3600>1 hour</a> |
+	$lnk=86400>1 day</a> |
+	$lnk=604800>7 days</a> |
+	$lnk=2592000>30 days</a> | 
+	$lnk=999999999>Total</a><br>
+	Posts $by$username in forums$during:
+".           "$L[TBL1]>
+".           "  $L[TRh]>
+".	"$L[TDh]>#
+".	"$L[TDh]>Forum
+".	"$L[TDh]>Posts
+".	"$L[TDh]>Forum total
+".      "  </tr>
+  ";
+  for($i=1;$f=$sql->fetch($posters);$i++){
+      if($i>1) $print.= '<tr>';
+	if(!(can_view_forum($f))) $link="<i>(Restricted forum)</i>";
+	else $link="<a href=forum.php?id=$f[id]>$f[title]</a>";
+      $print.= "
+".	"$L[TDc]>$i</td>
+".	"$L[TD]>$link</td>
+".	"$L[TDc]>$f[cnt]</td>
+".	"$L[TDc]>$f[posts]</td>
+".      "  </tr>
+      ";
+  }
+      $print.="$L[TBLend]";
+            $fpagelist="";
+  }
+
+  if(isset($_GET[postsbyforum])) {
+  if($id) pageheader("Posts in forums by user $user[name]");
+  else pageheader("Posts in forums on the board");
+  }
+  else if(isset($_GET[postsbythread])) {
+  pageheader("Posts in threads by user $user[name]");
+  }
+  else {
   pageheader("Posts by user $user[name]");
+  }
   echo $print.$fpagelist."<br>";
   pagefooter();
 ?>

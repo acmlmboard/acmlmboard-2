a985978949a5474c68cdc0e457cfa898bba3a081
SquidEmpress <squidempress2@gmail.com>
2015-03-06 03:23:53 +0000

    Added a permission for posting consecutively in a specified forum

---
+++ sql/updates/perm_update-150305.sql
@@ -0,0 +1,4 @@
+#Adds a permission for posting consecutively in specified forums.
+#Date: 3/05/2015
+
+INSERT INTO `perm` (`id`, `title`, `description`, `permcat_id`, `permbind_id`) VALUES ('create-consecutive-forum-post', 'Create Consecutive Forum Post', '', '2', 'forums');

84285c62c691a27354e42ce997a976b4417813ce
xeons <xeons@xeons.net>
2015-07-19 04:25:15 -0500

    moved login html to template file.

--- public_html/login.php
+++ public_html/login.php
@@ -2,60 +2,31 @@
 
 require 'lib/common.php';
 
-$rdmsg = "";
-if ($_COOKIE['pstbon']) {
-	header("Set-Cookie: pstbon=" . $_COOKIE['pstbon'] . "; Max-Age=1; Version=1");
-	$rdmsg = "<script language=\"javascript\">
-	function dismiss()
-	{
-		document.getElementById(\"postmes\").style['display'] = \"none\";
-	}
-</script>
-	<div id=\"postmes\" onclick=\"dismiss()\" title=\"Click to dismiss.\"><br>
-" . "<table cellspacing=\"0\" class=\"c1\" width=\"100%\" id=\"edit\"><tr class=\"h\"><td class=\"b h\">";
-	if ($_COOKIE['pstbon'] == -1) {
-		$rdmsg.="You are now registered!<div style=\"float: right\"><a style=\"cursor: pointer;\" onclick=\"dismiss()\">[x]</a></td></tr>
-" . "<tr><td class=\"b n1\" align=\"left\">Please login.</td></tr></table></div>";
-	}
-}
+$action = isset($_POST['action']) ? $_POST['action'] : '';
+switch($action) {
+	case 'login':
+		$f_username = isset($_POST['username']) ? $_POST['username'] : '';
+		$f_password = isset($_POST['password']) ? $_POST['password'] : '';
 
-$act = $_POST['action'];
-if ($act == 'Login') {
-	if ($userid = checkuser($_POST['name'], md5($pwdsalt2 . $_POST['pass'] . $pwdsalt))) {
-		setcookie('user', $userid, 2147483647);
-		setcookie('pass', packlcookie(md5($pwdsalt2 . $_POST[pass] . $pwdsalt), implode(".", array_slice(explode(".", $_SERVER['REMOTE_ADDR']), 0, 2)) . ".*"), 2147483647);
-		die(header("Location: ./"));
-	} else {
-		$err = "Invalid username or password, cannot log in.";
-	}
-	$print = "  <td class=\"b n1\" align=\"center\">$print</td>";
-} elseif ($act == 'logout') {
-	setcookie('user', 0);
-	setcookie('pass', '');
-	die(header("Location: ./"));
+		if ($userid = checkuser($f_username, md5($pwdsalt2 . $f_password . $pwdsalt))) {
+			setcookie('user', $userid, 2147483647);
+			setcookie('pass', packlcookie(md5($pwdsalt2 . $f_password . $pwdsalt), implode(".", array_slice(explode(".", $_SERVER['REMOTE_ADDR']), 0, 2)) . ".*"), 2147483647);
+			header("Location: ./");
+		} else {
+			$tpl_vars = array('error_message' => 'Invalid username or password.');
+			tpl_display('login', $tpl_vars);
+		}
+		break;
+	
+	case 'logout':
+		setcookie('user', 0);
+		setcookie('pass', '');
+		header("Location: ./");
+		break;
+	
+	default:
+		$tpl_vars = array();
+		tpl_display('login', $tpl_vars);
+		break;
 }
-
-pageheader('Login');
-if ($_COOKIE['pstbon']) {
-	print $rdmsg;
-}
-if ($err)
-	noticemsg("Error", $err);
-print "<table cellspacing=\"0\" class=\"c1\">
-<form action=login.php method=post>
-" . "  <tr class=\"h\">
-" . "    <td class=\"b h\" colspan=2>Login</td>
-" . "  <tr>
-" . "    <td class=\"b n1\" align=\"center\" width=120>Username:</td>
-" . "    <td class=\"b n2\"><input type=\"text\" name=name size=25 maxlength=25></td>
-" . "  <tr>
-" . "    <td class=\"b n1\" align=\"center\">Password:</td>
-" . "    <td class=\"b n2\"><input type=\"password\" name=pass size=13 maxlength=32> - <a href=\"resetpassword.php\">Lost password?</a></td>
-" . "  <tr class=\"n1\">
-" . "    <td class=\"b\">&nbsp;</td>
-" . "    <td class=\"b\"><input type=\"submit\" class=\"submit\" name=action value=Login></td>
-" . " </form>
-" . "</table>
-";
-pagefooter();
 ?>
---
+++ public_html/templates/login.html.php
@@ -0,0 +1,34 @@
+<?php 
+if (!function_exists('pageheader')) {
+	die('Access denied.');
+}
+
+pageheader('Login');
+
+if(isset($tpl_vars['error_message'])) {
+	noticemsg("Error", $tpl_vars['error_message']);
+}
+?>
+<form action="login.php" method="post">
+	<input type="hidden" name="action" value="login" />
+	<table cellspacing="0" class="c1">
+		<tr class="h">
+			<td class="b h" colspan="2">Login</td>
+		</tr>
+		<tr>
+			<td class="b n1" align="center" width="120">Username:</td>
+			<td class="b n2"><input type="text" name="name" size="25" maxlength="25" /></td>
+		</tr>
+		<tr>
+			<td class="b n1" align="center">Password:</td>
+			<td class="b n2"><input type="password" name="pass" size="13" maxlength="32" /> - <a href="resetpassword.php">Lost password?</a></td>
+		</tr>
+		<tr class="n1">
+			<td class="b">&nbsp;</td>
+			<td class="b"><input type="submit" class="submit" name="submit" value="Login" /></td>
+		</tr>
+	</table>
+</form>
+<?php
+pagefooter();
+?>

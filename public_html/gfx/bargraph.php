2134178b55718256c26b72b7a5c19e093cd91845
xeons <xeons@xeons.net>
2015-08-01 21:02:11 -0500

    Some notice fixes, removed some hardcoded paths on fonts, added color randomizer to bargraph if no color is specified.

--- public_html/gfx/bargraph.php
+++ public_html/gfx/bargraph.php
@@ -2,62 +2,67 @@
 
 require 'fontlib.php';
 
-$im=imagecreatetruecolor(690,20);
-$n=$_GET[n];
-$b=$_GET[b];
-$g=$_GET[g];
-$r=$_GET[r];
-$z=$_GET[z];
-if($n==0) $n=1;
+header('Content-type: image/png');
 
-imagealphablending($im,FALSE);
-imagesavealpha($im,TRUE);
+$n = isset($_GET['n']) ? (int) $_GET['n'] : 0;
+$z = isset($_GET['z']) ? (int) $_GET['z'] : 0;
 
-function cap($n)
-{
-return ($n>255?255:($n<0?0:$n));
+$b = isset($_GET['b']) ? (int) $_GET['b'] : mt_rand(0,255);
+$g = isset($_GET['g']) ? (int) $_GET['g'] : mt_rand(0,255);
+$r = isset($_GET['r']) ? (int) $_GET['r'] : mt_rand(0,255);
+
+if ($n == 0)
+	$n = 1;
+
+
+$im = imagecreatetruecolor(690, 20);
+
+imagealphablending($im, FALSE);
+imagesavealpha($im, TRUE);
+
+function cap($n) {
+	return ($n > 255 ? 255 : ($n < 0 ? 0 : $n));
 }
 
-$cblank=imagecolorallocatealpha($im,0,0,0,127);
-$cuhl=imagecolorallocate($im,cap($r+80),cap($g+80),cap($b+80));
-$chl=imagecolorallocate($im,cap($r+50),cap($g+50),cap($b+50));
-$cstd=imagecolorallocate($im,$r,$g,$b);
-$cshadow=imagecolorallocate($im,cap($r-80),cap($g-80),cap($b-80));
-$calpha=imagecolorallocatealpha($im,0,0,0,110);
-$calpha2=imagecolorallocatealpha($im,255,255,255,105);
-$cwhite=imagecolorallocate($im,255,255,255);
+$cblank = imagecolorallocatealpha($im, 0, 0, 0, 127);
+$cuhl = imagecolorallocate($im, cap($r + 80), cap($g + 80), cap($b + 80));
+$chl = imagecolorallocate($im, cap($r + 50), cap($g + 50), cap($b + 50));
+$cstd = imagecolorallocate($im, $r, $g, $b);
+$cshadow = imagecolorallocate($im, cap($r - 80), cap($g - 80), cap($b - 80));
+$calpha = imagecolorallocatealpha($im, 0, 0, 0, 110);
+$calpha2 = imagecolorallocatealpha($im, 255, 255, 255, 105);
+$cwhite = imagecolorallocate($im, 255, 255, 255);
 
 //imagecolortransparent($im,$cblank);
 
-imagefilledrectangle($im,0,0,690,20,$cblank);
+imagefilledrectangle($im, 0, 0, 690, 20, $cblank);
 
-imagefilledrectangle($im,0,5,600*$z/$n,15,$chl);
-imagefilledrectangle($im,1,6,600*$z/$n-1,14,$cstd);
+imagefilledrectangle($im, 0, 5, 600 * $z / $n, 15, $chl);
+imagefilledrectangle($im, 1, 6, 600 * $z / $n - 1, 14, $cstd);
 //imagefilledrectangle($im,0,2,600*$z/$n,3,$cuhl);
 //imagefilledrectangle($im,0,14,600*$z/$n,20,$cshadow);
 //imagefilledrectangle($im,600*$z/$n-1,0,600*$z/$n,20,$cshadow);
 
-$str="";
-$str=sprintf("%.1f%% ($z)",100*$z/$n);
+$str = "";
+$str = sprintf("%.1f%% ($z)", 100 * $z / $n);
 
 //imagettftext($im,10,0, floor(600*$z/$n)+3, 14, $cwhite, "/var/sites/acmlmboard/board/gfx/speculum.ttf", $str);
 
-$fontW=fontc(255,255,255, 210,210,210,  0, 0, 0);
-frender($im,$fontW, floor(600*$z/$n)+5, 7,0,$str);
+$fontW = fontc(255, 255, 255, 210, 210, 210, 0, 0, 0);
+frender($im, $fontW, floor(600 * $z / $n) + 5, 7, 0, $str);
 
-imagesavealpha($im,FALSE);
-imagealphablending($im,TRUE);
-for($i=0;$i<=600;$i+=10) {
-  imagefilledrectangle($im,$i+0,0,$i+4,20,$calpha);
-  imagefilledrectangle($im,$i+0,$i%60?18:($i%300?14:0),$i+0,20,$calpha2);
+imagesavealpha($im, FALSE);
+imagealphablending($im, TRUE);
+for ($i = 0; $i <= 600; $i+=10) {
+	imagefilledrectangle($im, $i + 0, 0, $i + 4, 20, $calpha);
+	imagefilledrectangle($im, $i + 0, $i % 60 ? 18 : ($i % 300 ? 14 : 0), $i + 0, 20, $calpha2);
 //  imagefilledrectangle($im,$i+5,5,$i+9,10,$calpha);
 //  imagefilledrectangle($im,$i+0,10,$i+4,14,$calpha);
 //  imagefilledrectangle($im,$i+5,15,$i+9,20,$calpha);
 //  imagefilledrectangle($im,0,$i,600,$i,$calpha);
 }
-imagealphablending($im,FALSE);
-imagesavealpha($im,TRUE);
+imagealphablending($im, FALSE);
+imagesavealpha($im, TRUE);
 
 imagepng($im);
-
 ?>
--- public_html/gfx/tpng.php
+++ public_html/gfx/tpng.php
@@ -1,33 +1,32 @@
 <?php
 
-$c=$_GET[c];
-$t=$_GET[t];
+$c = isset($_GET['c']) ? $_GET['c'] : 0;
+$t = isset($_GET['t']) ? (int)$_GET['t'] : 0;
 
-if(strstr($_SERVER['HTTP_USER_AGENT'],"MSIE")) {
+if (strstr($_SERVER['HTTP_USER_AGENT'], "MSIE")) {
 	header('Content-type: image/gif');
 
-	$im=imagecreatetruecolor(1,1);
+	$im = imagecreatetruecolor(1, 1);
 
-	$c=imagecolorallocate($im,0,0,0);
-	imagefilledrectangle($im,0,0,1,1,$c);
-	imagecolortransparent($im,$c);
+	$c = imagecolorallocate($im, 0, 0, 0);
+	imagefilledrectangle($im, 0, 0, 1, 1, $c);
+	imagecolortransparent($im, $c);
 
 	imagegif($im);
 }
 
 header('Content-type: image/png');
 
-$im=imagecreatetruecolor(1,1);
+$im = imagecreatetruecolor(1, 1);
 
-imagealphablending($im,FALSE);
-imagesavealpha($im,TRUE);
+imagealphablending($im, FALSE);
+imagesavealpha($im, TRUE);
 
-sscanf($c,"%02X%02X%02X",&$r,&$g,&$b);
+sscanf($c, "%02X%02X%02X", &$r, &$g, &$b);
 
-$c=imagecolorallocatealpha($im,$r,$g,$b,$t);
+$c = imagecolorallocatealpha($im, $r, $g, $b, $t);
 
-imagefilledrectangle($im,0,0,1,1,$c);
+imagefilledrectangle($im, 0, 0, 1, 1, $c);
 
 imagepng($im);
-
 ?>

bafd931d7206883ddeb3d0d4faff5f106614efcb
xeons <xeons@xeons.net>
2015-08-04 01:32:41 -0500

    removed broken includes

--- public_html/gfx/gfxlib.php
+++ public_html/gfx/gfxlib.php
@@ -1,9 +1,7 @@
 <?php
-require '../lib/datetime.php';
 require '../lib/helpers.php';
 require '../lib/config.php';
 require '../lib/auth.php';
-require '../lib/request.php';
 require '../lib/classes/mysql.php';
 require '../lib/database.php';
 require '../lib/rpg.php';

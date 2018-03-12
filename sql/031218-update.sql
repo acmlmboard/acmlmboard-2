a9d66bd48aa0ae609b1c05c3717ff174f5acb13d
Epele <draobyek@googlemail.com>
2018-03-12 22:38:56 +0000

    Forgot to include enable-by-default for normal users.

--- sql/031218-update.sql
+++ sql/031218-update.sql
@@ -1,1 +1,2 @@
-INSERT INTO `kafuka`.`perm` (`id`, `title`, `description`, `permcat_id`, `permbind_id`) VALUES ('edit-smilies', 'Update Smilies', '', '3', 'users'), ('edit-thread', 'Edit Own Thread/Poll', '', '2', 'users');
+INSERT INTO `perm` (`id`, `title`, `description`, `permcat_id`, `permbind_id`) VALUES ('edit-smilies', 'Update Smilies', '', '3', 'users'), ('edit-thread', 'Edit Own Thread/Poll', '', '2', 'users');
+INSERT INTO `x_perm` (`id`, `x_id`, `x_type`, `perm_id`, `permbind_id`, `bindvalue`, `revoke`) VALUES (NULL, 2, 'group', 'edit-thread', '', 0, 0);

516f8071e34e58699a3e80f48ed42b2b48abc558
Tom Rinaldo <emuz@emuz.us>
2018-04-08 21:08:50 -0400

    Acmlmboard 2.5.4 Release

--- public_html/lib/common.php
+++ public_html/lib/common.php
@@ -6,7 +6,7 @@
   //[Scrydan] Added these three variables to make editing quicker.
   $boardprog = "Acmlm, Emuz, <a href='credits.php'>et al</a>.";
   $abdate    = "4/09/2018";
-  $abversion = "2.5.4 <span style=\"color: #BCDE9A; font-style: italic;\">Development</span>";
+  $abversion = "2.5.4";
 
   $userip  = $_SERVER['REMOTE_ADDR'];
   $userfwd = addslashes(getenv('HTTP_X_FORWARDED_FOR')); //We add slashes to that because the header is under users' control

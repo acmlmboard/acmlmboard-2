411d979977142110830656dcd411346287372e4d
Kak2X <kak32x@gmail.com>
2018-04-21 02:36:04 +0200

    Improve post revision diff page

--- public_html/postdiff.php
+++ public_html/postdiff.php
@@ -1,62 +1,119 @@
 <?php
-include('lib/common.php');
-include('lib/diff/Diff.php');
-include('lib/diff/Diff/Renderer/inline.php');
+require 'lib/common.php';
 
-$pid=(int)$_GET['id'];
-$r1=(int)$_GET['o'];
-$r2=(int)$_GET['n'];
+$pid  = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+$r1   = isset($_GET['o'])  ? (int)$_GET['o']  : 0;
+$r2   = isset($_GET['n'])  ? (int)$_GET['n']  : 0;
+$mode = isset($_GET['m'])  ? (int)$_GET['m'] : 0;
 
 $t = $sql->resultq("SELECT thread FROM posts WHERE id=$pid");
-if(!$t) { error("Error", "This post does not exist.<br> <a href=./>Back to main</a>"); }
+if (!$t) 
+	error("Error", "This post does not exist.<br> <a href=./>Back to main</a>");
 $f = $sql->resultq("SELECT forum FROM threads WHERE id=$t");
-if(!can_view_forum_post_history($f) || !can_view_forum($f)) { error("Error", "You have no permissions to do this!<br> <a href=./>Back to main</a>"); }
+if (!can_view_forum_post_history($f) || !can_view_forum($f))
+	error("Error", "You have no permissions to do this!<br> <a href=./>Back to main</a>");
 
-pageheader("Post revision differences");
 
-if(!$r1||!$r2) $r1=$r2=1;
+// Get the post revisions we need
+if (!$r1 || !$r2) $r1 = $r2 = 1;
+$text = $sql->getresultsbykey("SELECT revision, text FROM poststext WHERE id = {$pid} AND revision IN ({$r1}, {$r2})", 'revision', 'text');
+if (!isset($text[$r1]) || !isset($text[$r2]))
+	error("Error", "One of the revisions you selected does not exist. <br> <a href=./>Back to main</a>"); 
 
-$d1=$sql->fetchq("SELECT text FROM poststext WHERE id=$pid AND revision=$r1");
-$d2=$sql->fetchq("SELECT text FROM poststext WHERE id=$pid AND revision=$r2");
-
-echo "$L[TBL1] width=100% height=100>$L[TR1]>$L[TD2]><font face='courier new'>";
-
-$diff = &new Text_Diff("native",array(explode("\n",$d1[text]),explode("\n",$d2[text])));
-
-?>
-<style type=text/css>
-del {
-	text-decoration: none;
-	background-color: #800000;
-	border: 1px dashed #FF0000;
-	color: #cfcfcf;
-	margin-left:1px;
-	padding-left:1px;
-	padding-right:1px;
+// This diff library provides a few different modes
+require 'lib/diff/autoload.php';
+switch ($mode) {
+	case 0: $granularity = new cogpowered\FineDiff\Granularity\Character; break;
+	case 1: $granularity = new cogpowered\FineDiff\Granularity\Word;      break;
+	case 2: $granularity = new cogpowered\FineDiff\Granularity\Sentence;  break;
+	case 3: $granularity = new cogpowered\FineDiff\Granularity\Paragraph; break;
+	default: error("Error", "No.");
 }
-ins {
-	text-decoration: none;
-	background-color: #008000;
-	border: 1px dashed #00FF00;
-	color: #ffffff;
-	margin-left:1px;
-	padding-left:1px;
-	padding-right:1px;
-}
-</style>
-<?php
+$diff = new cogpowered\FineDiff\Diff($granularity);
 
-$renderer = &new Text_Diff_Renderer_inline();
+
 //What is this? I don't even…
 /*if($act=="hs") {
 	$ip=$sql->fetchq("SELECT ip FROM users WHERE id=$pid");
 	$ip=$ip[0];
 	echo $ip." = ".gethostbyaddr($ip);
-} else*/ echo str_replace("\n","<br>",$renderer->render($diff));
+}*/
 
-//echo diff(str_replace("\n","<br>\n",$d1[text])."\n",str_replace("\n","<br>\n",$d2[text])."\n");
-echo "$L[TBLend]";
+// Revision jump
+$revs = $sql->query("SELECT revision FROM poststext WHERE id = {$pid}");
+$oldrev = "";
+$newrev = "";
+while ($x = $sql->fetch($revs)) {
+	$w1 = ($x['revision'] == $r1) ? "b" : "a";
+	$w2 = ($x['revision'] == $r2) ? "b" : "a";
+	$oldrev .= "<{$w1} href='?id={$pid}&o={$x['revision']}&n={$r2}&m={$mode}'>{$x['revision']}</{$w1}> ";
+	$newrev .= "<{$w2} href='?id={$pid}&o={$r1}&n={$x['revision']}&m={$mode}'>{$x['revision']}</{$w2}> ";
+}
 
-pagefooter();
+pageheader("Post revision differences");
 
 ?>
+<style type="text/css">
+	#compare {
+		font-family: "Courier New", Courier, monospace;
+	}
+</style>
+
+<!-- dumb trick with displaying / hiding elements -->
+<style type="text/css" id="diffmode"></style>
+<script type="text/javascript">
+	var diffcss = document.getElementById('diffmode');
+	function showdiff(mode) {
+		switch (mode) {
+			case 0: diffcss.innerHTML = ""; break;
+			case 1: diffcss.innerHTML = "ins {display: none}"; break;
+			case 2: diffcss.innerHTML = "del {display: none}"; break;
+		}
+	}
+</script>
+<?php
+
+print "
+$L[TBL1]>
+	$L[TRh]>$L[TDh] colspan=2>Revision diff for post #{$pid}</td></tr>
+	$L[TR1]>
+		$L[TD1c] style='width: 150px'><b>Source revision:</b></td>
+		$L[TD2]>{$oldrev}</td>
+	</tr>
+	$L[TR1]>
+		$L[TD1c]><b>Compare to:</b></td>
+		$L[TD2]>{$newrev}</td>
+	</tr>
+	$L[TR1]>
+		$L[TD1c]><b>Diff mode:</b></td>
+		$L[TD2]>
+			<a href='?id={$pid}&o={$r1}&n={$r2}&m=0'>Character</a> - 
+			<a href='?id={$pid}&o={$r1}&n={$r2}&m=1'>Word</a> - 
+			<a href='?id={$pid}&o={$r1}&n={$r2}&m=2'>Sentence</a> - 
+			<a href='?id={$pid}&o={$r1}&n={$r2}&m=3'>Paragraph</a>
+		</td>
+	</tr>
+	$L[TR1]>
+		$L[TD1c]><b>Show:</b></td>
+		$L[TD2]>
+			<label>$L[INPr]=diffmode onclick='showdiff(0)' id='default' checked> Diff</label>
+			<label>$L[INPr]=diffmode onclick='showdiff(1)'> Source revision</label>
+			<label>$L[INPr]=diffmode onclick='showdiff(2)'> Dest revision</label>
+		</td>
+	</tr>
+	$L[TRh]>$L[TDh] colspan=2 style='height: 5px'></td></tr>
+	$L[TR2]>
+		$L[TD1c]><b>Result:</b></td>
+		$L[TD2] id='compare'>".nl2br($diff->render($text[$r1], $text[$r2]))."</td>
+	</tr>
+</table>
+";
+
+// Default to first element because browsers preserve choices on soft refresh, even when it doesn't make sense
+?>
+<script type="text/javascript">
+	document.getElementById('default').checked=true;
+</script>
+<?php
+
+pagefooter();

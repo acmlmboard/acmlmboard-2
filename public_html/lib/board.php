7d3c6facd7e2ce83e73ec2505dc0df040179af4f
Epele <draobyek@googlemail.com>
2018-04-22 20:44:29 +0100

    Fix for flags jumping to upper case as per DB definitions.

--- public_html/lib/board.php
+++ public_html/lib/board.php
@@ -7,7 +7,7 @@
 	  global $sql; 
 	  $flag = $sql->resultq("SELECT cc2 FROM ip2c WHERE ip_from<=inet_aton('{$ip}') AND ip_to>=inet_aton('{$ip}')");
 	} 
-    return ($flag != '-' ?" <img src=\"img/flags/{$flag}.png\" style='width: 16px; height: 11px; float: right'>" : "") . $ip . " <small>[<a href='http://google.com/search?q={$ip}'>G</a>]</small>";
+    return ($flag != '-' ?" <img src=\"img/flags/".strtolower($flag).".png\" style='width: 16px; height: 11px; float: right'>" : "") . $ip . " <small>[<a href='http://google.com/search?q={$ip}'>G</a>]</small>";
   }
 
   function feedicon($icon,$para,$text="RSS feed"){

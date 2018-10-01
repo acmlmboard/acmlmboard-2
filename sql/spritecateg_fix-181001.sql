c02e633835da5d68d1a33fff1fc943bbbb46d893
Epele <draobyek@googlemail.com>
2018-10-01 19:17:22 +0100

    Fixed main.sql missing index and auto-increment on id field. Supplied separate spritecateg_fix-181001.sql file for existing boards. This should fix issues with creating sprite categories.

---
+++ sql/spritecateg_fix-181001.sql
@@ -0,0 +1,15 @@
+--
+-- Indexes for table `spritecateg`
+--
+ALTER TABLE `spritecateg`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `spritecateg`
+--
+ALTER TABLE `spritecateg`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;

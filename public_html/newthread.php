e88a45a45456bd8b8f69836bc240198201bc9175
Epele <draobyek@googlemail.com>
2019-03-04 23:47:44 +0000

    Bugfix for polls

--- public_html/newthread.php
+++ public_html/newthread.php
@@ -129,7 +129,7 @@ if($act!="Submit"){
     print "$top - Error";
     noticemsg("Error", $err);
   }elseif(!$act){
-    if(checkvar('$ispoll')){
+    if(checkvar('ispoll')){
       $pollin=
           "$L[TR]>
 ".        "  $L[TD1c]>Poll question:</td>

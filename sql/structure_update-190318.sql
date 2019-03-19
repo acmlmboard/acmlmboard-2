017810954f65caf33fe8360e7411b4a49cb4f9f7
Epele <draobyek@googlemail.com>
2019-03-19 00:52:42 +0000

    Overlooked that profileext is in main sql. uncommented.

--- sql/structure_update-190318.sql
+++ sql/structure_update-190318.sql
@@ -2,4 +2,4 @@ ALTER TABLE `smilies` ADD `id` INT(16) NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST
 ALTER TABLE `robots` ADD `id` INT(16) NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;
 
 # This is an alteration for future-proofing purposes.
-#ALTER TABLE `profileext` CHANGE `id` `service` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
+ALTER TABLE `profileext` CHANGE `id` `service` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';

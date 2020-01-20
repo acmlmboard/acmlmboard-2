05e850b267b49d5d5cf8bfbe17648fbbc23a98ef
Epele <draobyek@googlemail.com>
2020-01-20 18:13:00 +0000

    Notice fixed.

--- public_html/gfx/userpic.php
+++ public_html/gfx/userpic.php
@@ -1,6 +1,6 @@
 <?php
     $file='../userpic/'.$_GET['id'];
-  if($_GET['r']) $r="?r=".$_GET['r'];
+  if(isset($_GET['r'])) $r="?r=".$_GET['r'];
   if((is_numeric($_GET['id']) || preg_match("/\d+_\d\d?/", $_GET['id'])) && file_exists($file))
     Header("Location:$file$r");
   else

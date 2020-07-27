d5df1641ffad2823d18d8c2a8642150227125348
Emuz <emuz@emuz.us>
2020-07-27 18:24:11 -0400

    Updated database to give tags more room to breath

--- sql/main.sql
+++ sql/main.sql
@@ -1896,3 +1896,4 @@ ALTER TABLE `robots` ADD `id` INT(16) NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;
 
 # This is an alteration for future-proofing purposes.
 ALTER TABLE `profileext` CHANGE `id` `service` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
+ALTER TABLE `threads` CHANGE `tags` `tags` BIGINT(12) NOT NULL;

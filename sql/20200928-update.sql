8758a080540d405f8abeb97ec61656e3f5140e65
SorceressEpele <draobyek@googlemail.com>
2020-09-28 20:03:51 +0100

    sql-auto update to add permission data for Can PM Staff/Admins.

---
+++ sql/20200928-update.sql
@@ -0,0 +1,2 @@
+INSERT INTO `perm` (`id`, `title`, `description`, `permcat_id`, `permbind_id`) VALUES ('create-admin-pms', 'Can PM Staff/Admins', '', '1', '');
+UPDATE `misc` SET `sqlversion`=20200928;

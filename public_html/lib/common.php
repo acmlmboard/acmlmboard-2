57c6aeb47561d76beb2a4860327cb2c5a83e48a1
SorceressEpele <draobyek@googlemail.com>
2024-06-11 01:29:43 +0100

    Update date/version

--- public_html/lib/common.php
+++ public_html/lib/common.php
@@ -11,8 +11,8 @@
   
   //[Scrydan] Added these three variables to make editing quicker.
   $boardprog = "Acmlm, Emuz, <a href='credits.php'>et al</a>.";
-  $abdate    = "10/04/2020";
-  $abversion = "2.5.5<i>pre</i> <span style=\"color: #BCDE9A; font-style: italic;\">Development</span>";
+  $abdate    = "06/11/2024";
+  $abversion = "2.5.6<i>pre</i> <span style=\"color: #BCDE9A; font-style: italic;\">Development</span>";
 
   $userip  = $_SERVER['REMOTE_ADDR'];
   $userfwd = addslashes(getenv('HTTP_X_FORWARDED_FOR')); //We add slashes to that because the header is under users' control
